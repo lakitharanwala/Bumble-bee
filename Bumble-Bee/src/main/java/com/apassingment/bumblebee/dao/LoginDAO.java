bb9caf63e619e5d51516390a499fc4a5eb0860b7
lakitha ranwala <lakitharanwala@gmail.com>
2023-04-06 09:39:09 +0530

    role validation added

--- Bumble-Bee/src/main/java/com/apassingment/bumblebee/dao/LoginDAO.java
+++ Bumble-Bee/src/main/java/com/apassingment/bumblebee/dao/LoginDAO.java
@@ -7,6 +7,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.apassingment.bumblebee.model.Products;
+import com.apassingment.bumblebee.model.User;
+
 public class LoginDAO {
 
 	public DbConnector getConnector() {
@@ -26,15 +29,37 @@ public class LoginDAO {
 		boolean status=false; 
 		Connection connection =connection();
 		
-		String quary="Select * from user where user_name =? and pasword = ?";
-		PreparedStatement ps=connection.prepareStatement(quary);
+		String quary="Select * from user where user_name =? and pasword = ? ";
+//		PreparedStatement ps=connection.prepareStatement(quary);
+//		
+//		ps.setString(1, userName);
+//		ps.setString(2, password);
+//		ps.setString(3, "1");
 		
+		
+		PreparedStatement ps=connection.prepareStatement(quary);
 		ps.setString(1, userName);
 		ps.setString(2, password);
 		
-		ResultSet rs=ps.executeQuery();  
-		status=rs.next();  
+		ResultSet rs=ps.executeQuery();
+		User user=new User();
+		while(rs.next()){
+			
+			user.setUsername(userName);
+			user.setUserRole(rs.getString("user_role"));
+			user.setPassword(password);
+			}
 		
+		if(user.getUserRole().equals("1")) {
+			status=true;
+		}
+		
+//		ResultSet rs=ps.executeQuery();  
+//		status=rs.next();  
+//		
+//		if() {
+//			
+//		}
 		
 		return status;  
 		
